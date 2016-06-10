eccf5ea71440d77afcaf20008bb5cb6d6b3f65ad
Peter Pult <p.pult@gini.net>
2016-06-10 15:41:59 +0200

    Update docs script

--- scripts/push-documentation.sh
+++ scripts/push-documentation.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -e # Exit with nonzero exit code if anything fails
 
-# Pull requests and commits to other branches shouldn't try to deploy
-# if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "master" ]; then
-#     echo "Skipping documentation deploy."
-#     exit 0
-# fi
+Pull requests and commits to other branches shouldn't try to deploy
+if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "master" ]; then
+    echo "Skipping documentation deploy."
+    exit 0
+fi
 
 git checkout docs
 git merge master -X theirs --no-ff --no-commit
