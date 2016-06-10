50b0b61b6005dcf3385ebfc59d6b532f7b1eb030
Peter Pult <p.pult@gini.net>
2016-06-10 16:13:10 +0200

    Git clone instead of merge

--- scripts/push-documentation.sh
+++ scripts/push-documentation.sh
@@ -2,14 +2,26 @@
 set -e # Exit with nonzero exit code if anything fails
 
 # Pull requests and commits to other branches shouldn't try to deploy
-if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "master" ]; then
-    echo "Skipping documentation deploy."
-    exit 0
-fi
+# if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "master" ]; then
+#     echo "Skipping documentation deploy."
+#     exit 0
+# fi
 
-git checkout docs
-git merge master -X theirs --no-ff --no-commit
-git reset 
+# git checkout docs
+# git merge master -X theirs --no-ff --no-commit
+# git reset 
+# git add .
+# git commit -m "Update docs branch" 
+# git push origin docs
+
+rm -rf docs
+git clone -b docs git@github.com:gini/gini-vision-lib-ios.git docs
+
+rm -rf docs/*
+cp -a Documentation/. docs/
+cd docs
+
+git add -u
 git add .
-git commit -m "Update docs branch" 
-git push origin docs
+git diff --quiet --exit-code --cached || git commit -a -m 'Deploy Gini Vision Library for iOS documentation to docs branch'
+git push
