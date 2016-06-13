9b98a13ae0df8ce2671c8ecec49b82cf1b28489a
Peter Pult <p.pult@gini.net>
2016-06-13 18:39:12 +0200

    Remove branch check from deploy scripts and only rely on tags for now

--- scripts/push-pod.sh
+++ scripts/push-pod.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e # Exit with nonzero exit code if anything fails
 
-# Pull requests and commits to other branches shouldn't try to deploy
-if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "master" -o -z "$TRAVIS_TAG" ]; then
+# Pull requests and non-tagged commits shouldn't try to deploy
+if [ "$TRAVIS_PULL_REQUEST" != "false" -o -z "$TRAVIS_TAG" ]; then
 	echo "branch:$TRAVIS_BRANCH;tag:$TRAVIS_TAG;pullRequest:$TRAVIS_PULL_REQUEST"
     echo "Skipping documentation deploy."
     exit 0
