8a25b182651241071f741ef4e0fe0cc129c10334
Peter Pult <p.pult@gini.net>
2016-06-13 18:27:19 +0200

    Fix release script conditions

--- scripts/push-documentation.sh
+++ scripts/push-documentation.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 set -e # Exit with nonzero exit code if anything fails
 
-# Pull requests and untagged commits to other branches shouldn't try to deploy
-if ([ "$TRAVIS_BRANCH" != "master" ] || [ -z "$TRAVIS_TAG" ]) || [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
+# Pull requests and commits to other branches shouldn't try to deploy
+if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "master" -o -z "$TRAVIS_TAG" ]; then
+	echo "branch:$TRAVIS_BRANCH;tag:$TRAVIS_TAG;pullRequest:$TRAVIS_PULL_REQUEST"
     echo "Skipping documentation deploy."
     exit 0
 fi
