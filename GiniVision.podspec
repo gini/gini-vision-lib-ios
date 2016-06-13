27664857791d766b5b83c0aef576db72339afe56
Peter Pult <p.pult@gini.net>
2016-06-13 17:59:30 +0200

    Update version to 0.1.1

--- GiniVision.podspec
+++ GiniVision.podspec
@@ -8,7 +8,7 @@
 
 Pod::Spec.new do |s|
   s.name             = 'GiniVision'
-  s.version          = '0.1.0'
+  s.version          = '0.1.1'
   s.summary          = 'Computer Vision Library for scanning documents.'
 
 # This description is used to generate tags and improve search results.
--- scripts/push-pod.sh
+++ scripts/push-pod.sh
@@ -3,7 +3,7 @@ set -e # Exit with nonzero exit code if anything fails
 
 # Pull requests and untagged commits to other branches shouldn't try to deploy
 if ([ "$TRAVIS_BRANCH" != "master" ] || [ -z "$TRAVIS_TAG" ]) || [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
-    echo "Skipping documentation deploy."
+    echo "Skipping pod deploy."
     exit 0
 fi
 
