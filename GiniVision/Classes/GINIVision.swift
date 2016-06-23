05be877f1f597baddb7bac9d0bdebff871df8ede
Peter Pult <p.pult@gini.net>
2016-06-23 12:42:36 +0200

    Add docs to `GINIVision` class

--- GiniVision/Classes/GINIVision.swift
+++ GiniVision/Classes/GINIVision.swift
@@ -8,19 +8,55 @@
 
 import Foundation
 
+/**
+ *  Delegate to inform the reveiver about the current status of the Gini Vision Library.
+ *  Make use of callbacks for handling incoming data and to control view controller presentation.
+ *  
+ *  - note: ScreenAPI only
+ */
 @objc public protocol GINIVisionDelegate {
     
+    /**
+     Called when the user took an image.
+     
+     - parameter imageData: JPEG image data including meta information.
+     */
     func didCapture(imageData: NSData)
+    
+    /**
+     Called when the user reviewed the image and potentially rotated it to reading order.
+     
+     - parameter imageData: JPEG image data including eventually updated meta information.
+     - parameter changes:   Indicates whether `imageData` was altered.
+    */
     func didReview(imageData: NSData, withChanges changes: Bool)
+    
+    /**
+     Called when the user canceles capturing on the camera screen. Should be used to dismiss the presented view controller.
+     */
     func didCancelCapturing()
     
+    /**
+     Called when the user navigates back from the review screen to the camera potentially to retake an image. Should be used to cancel any ongoing analysis task on the image.
+     */
     optional func didCancelReview()
+    
+    /**
+     Called when the user navigates back from the analysis screen to the review screen. Should be used to cancel any ongoing analysis task on the image.
+     */
     optional func didCancelAnalysis()
     
 }
 
+/// Convenience class to interact with the Gini Vision Library.
 @objc public final class GINIVision: NSObject {
     
+    /**
+     Sets a configuration which is used to customize the look and feel of the Gini Vision Library.
+     For example to change texts and colors displayed to the user.
+     
+     - parameter configuration: The configuration to set.
+     */
     public class func setConfiguration(configuration: GINIConfiguration) {
         if configuration.debugModeOn {
             print("GiniVision: Set mode to DEBUG (WARNING: Never make a release in DEBUG mode!)")
@@ -28,6 +64,15 @@ import Foundation
         GINIConfiguration.sharedConfiguration = configuration // TODO: Make copy
     }
     
+    /**
+     Returns a navigation view controller with the camera screen loaded and ready to go. It's the easiest way to get started with the Gini Vision Library as it comes pre-configured and handles all screens and transitions out of the box.
+     
+     - note: ScreenAPI only
+     
+     - parameter delegate: An instance conforming to the `GINIVisionDelegate` protocol.
+     
+     - returns: A presentable navigation view controller.
+     */
     public class func viewController(withDelegate delegate: GINIVisionDelegate) -> UIViewController {
         let cameraContainerViewController = GINICameraContainerViewController()
         let navigationController = GININavigationViewController(rootViewController: cameraContainerViewController)
@@ -35,6 +80,16 @@ import Foundation
         return navigationController
     }
     
+    /**
+     Returns a navigation view controller with the camera screen loaded and ready to go. Allows to set a custom conifugration to set change the lookg and feel of the Gini Vision Library.
+     
+     - note: ScreenAPI only
+     
+     - parameter delegate:      An instance conforming to the `GINIVisionDelegate` protocol.
+     - parameter configuration: The configuration to set.
+     
+     - returns: A presentable navigation view controller.
+     */
     public class func viewController(withDelegate delegate: GINIVisionDelegate, withConfiguration configuration: GINIConfiguration) -> UIViewController {
         setConfiguration(configuration)
         return viewController(withDelegate: delegate)
