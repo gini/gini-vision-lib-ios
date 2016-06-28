ccbe599319607e7fe30c2efc51627b45a9155592
Peter Pult <p.pult@gini.net>
2016-06-28 02:54:29 +0200

    Add a note on the Component API flow

--- GiniVision/Classes/GINIVision.swift
+++ GiniVision/Classes/GINIVision.swift
@@ -56,6 +56,8 @@ import Foundation
  By integrating this library in your application you can allow your users to easily take pictures of documents, review them and - by implementing the necessary callbacks - upload the document for analysis to the Gini API.
  
  The Gini Vision Library can be integrated in two ways, either by using the **Screen API** or the **Component API**. The Screen API provides a fully pre-configured navigation controller for easy integration, while the Component API provides single view controllers for advanced integration with more freedom for customisation.
+ 
+ - important: When using the Component API we advise you to use a similar flow as suggested in the Screen API. Use the `GINICameraViewController` as an entry point with the `GINIOnboardingViewController` presented on top of it. After capturing let the user review the document with the `GINIReviewViewController` and finally present the `GINIAnalysisViewController` while the user waits for the analysis results.
  */
 @objc public final class GINIVision: NSObject {
     
