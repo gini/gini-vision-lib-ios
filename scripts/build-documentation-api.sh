645db7828968002958a90d89a7ac816b035c9e0e
Nikola <n.sobadjiev@gini.net>
2017-01-13 10:56:51 +0100

    Workaround for jazzy installation error in build_documentation-api

--- scripts/build-documentation-api.sh
+++ scripts/build-documentation-api.sh
@@ -2,7 +2,6 @@
 set -e # Exit with nonzero exit code if anything fails
 
 # Start with a "cleaner" sheet
-gem uninstall json --version '>2.0.0'
 gem uninstall bundler -v '>1.12.5' --force || echo "bundler >1.12.5 is not installed"
 gem install bundler -v 1.12.5 --no-rdoc --no-ri --no-document --quiet
 
