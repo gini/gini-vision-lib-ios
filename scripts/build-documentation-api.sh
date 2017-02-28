1afe90077a35d234a3588ee9d8a645020f61d813
Nikola <n.sobadjiev@gini.net>
2017-02-28 12:34:11 +0100

    Commented json and bundler installation steps from build-documentation-api

--- scripts/build-documentation-api.sh
+++ scripts/build-documentation-api.sh
@@ -2,9 +2,9 @@
 set -e # Exit with nonzero exit code if anything fails
 
 # Start with a "cleaner" sheet
-gem uninstall json -a --version '>2.0.0'
-gem uninstall bundler -v '>1.12.5' --force || echo "bundler >1.12.5 is not installed"
-gem install bundler -v 1.12.5 --no-rdoc --no-ri --no-document --quiet
+# gem uninstall json -a --version '>2.0.0'
+# gem uninstall bundler -v '>1.12.5' --force || echo "bundler >1.12.5 is not installed"
+# gem install bundler -v 1.12.5 --no-rdoc --no-ri --no-document --quiet
 
 gem install jazzy
 jazzy -v
