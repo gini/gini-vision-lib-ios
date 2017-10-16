217f0c78d27d95cc822d35b63dbf1a26ad4d388a
Enrique <enrique0703@gmail.com>
2017-10-16 11:21:56 +0200

    Added iPad layout for suggestion collection view.
    Issue #69

--- GiniVision/Classes/NoResultsViewController.swift
+++ GiniVision/Classes/NoResultsViewController.swift
@@ -29,7 +29,7 @@ public final class NoResultsViewController: UIViewController {
         text.textColor = .white
         return text
     }()
-    fileprivate var tipsCollectionView: CaptureSuggestionsCollectionView = CaptureSuggestionsCollectionView()
+    fileprivate var suggestionsCollectionView: CaptureSuggestionsCollectionView = CaptureSuggestionsCollectionView()
     fileprivate var repeatAnalysisButton: UIButton = {
         let closeButtonResources = PreferredButtonResource(image: "navigationCameraClose", title: "ginivision.navigationbar.camera.close", comment: "Button title in the navigation bar for the close button on the camera screen", configEntry: GiniConfiguration.sharedConfiguration.navigationBarCameraTitleCloseButton)
         let repeatButton = UIButton()
@@ -41,9 +41,9 @@ public final class NoResultsViewController: UIViewController {
     
     fileprivate let captureTips: [(image: UIImage?, text: String)] = [
         (UIImageNamedPreferred(named: "onboardingTip1"), NSLocalizedString("ginivision.analysis.suggestion.1", bundle: Bundle(for: GiniVision.self), comment: "First suggestion text for analysis screen")),
-        (UIImageNamedPreferred(named: "onboardingTip2"), NSLocalizedString("ginivision.analysis.suggestion.2", bundle: Bundle(for: GiniVision.self), comment: "Second suggestion text for analysis screen")),
         (UIImageNamedPreferred(named: "onboardingTip3"), NSLocalizedString("ginivision.analysis.suggestion.3", bundle: Bundle(for: GiniVision.self), comment: "Third suggestion text for analysis screen")),
-        (UIImageNamedPreferred(named: "onboardingTip4"), NSLocalizedString("ginivision.analysis.suggestion.4", bundle: Bundle(for: GiniVision.self), comment: "Forth suggestion text for analysis screen"))
+        (UIImageNamedPreferred(named: "onboardingTip4"), NSLocalizedString("ginivision.analysis.suggestion.4", bundle: Bundle(for: GiniVision.self), comment: "Forth suggestion text for analysis screen")),
+        (UIImageNamedPreferred(named: "onboardingTip2"), NSLocalizedString("ginivision.analysis.suggestion.2", bundle: Bundle(for: GiniVision.self), comment: "Second suggestion text for analysis screen"))
     ]
     
     override public func loadView() {
@@ -51,21 +51,27 @@ public final class NoResultsViewController: UIViewController {
         view = UIView()
         view.backgroundColor = .white
         
-        tipsCollectionView.dataSource = self
-        tipsCollectionView.delegate = self
+        suggestionsCollectionView.dataSource = self
+        suggestionsCollectionView.delegate = self
         
         warningViewContainer.addSubview(warningViewIcon)
         warningViewContainer.addSubview(warningViewText)
         view.addSubview(warningViewContainer)
-        view.addSubview(tipsCollectionView)
+        view.addSubview(suggestionsCollectionView)
         view.addSubview(repeatAnalysisButton)
         
         addConstraints()
         addConstraintsWarningView()
     }
     
+    public override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+        coordinator.animate(alongsideTransition: { _ in
+            self.suggestionsCollectionView.collectionViewLayout.invalidateLayout()
+        }, completion: nil)
+    }
+    
     fileprivate func addConstraints() {
-        tipsCollectionView.translatesAutoresizingMaskIntoConstraints = false
+        suggestionsCollectionView.translatesAutoresizingMaskIntoConstraints = false
         repeatAnalysisButton.translatesAutoresizingMaskIntoConstraints = false
         
         ConstraintUtils.addActiveConstraint(item: self.view, attribute: .bottom, relatedBy: .equal, toItem: repeatAnalysisButton, attribute: .bottom, multiplier: 1.0, constant: 0)
@@ -73,10 +79,10 @@ public final class NoResultsViewController: UIViewController {
         ConstraintUtils.addActiveConstraint(item: self.view, attribute: .trailing, relatedBy: .equal, toItem: repeatAnalysisButton, attribute: .trailing, multiplier: 1.0, constant: 0)
         ConstraintUtils.addActiveConstraint(item: repeatAnalysisButton, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1.0, constant: 60)
         
-        ConstraintUtils.addActiveConstraint(item: tipsCollectionView, attribute: .top, relatedBy: .equal, toItem: warningViewContainer, attribute: .bottom, multiplier: 1.0, constant: 0)
-        ConstraintUtils.addActiveConstraint(item: tipsCollectionView, attribute: .bottom, relatedBy: .equal, toItem: repeatAnalysisButton, attribute: .top, multiplier: 1.0, constant: 0)
-        ConstraintUtils.addActiveConstraint(item: self.view, attribute: .leading, relatedBy: .equal, toItem: tipsCollectionView, attribute: .leading, multiplier: 1.0, constant: 0)
-        ConstraintUtils.addActiveConstraint(item: self.view, attribute: .trailing, relatedBy: .equal, toItem: tipsCollectionView, attribute: .trailing, multiplier: 1.0, constant: 0)
+        ConstraintUtils.addActiveConstraint(item: suggestionsCollectionView, attribute: .top, relatedBy: .equal, toItem: warningViewContainer, attribute: .bottom, multiplier: 1.0, constant: 0)
+        ConstraintUtils.addActiveConstraint(item: suggestionsCollectionView, attribute: .bottom, relatedBy: .equal, toItem: repeatAnalysisButton, attribute: .top, multiplier: 1.0, constant: 0)
+        ConstraintUtils.addActiveConstraint(item: self.view, attribute: .leading, relatedBy: .equal, toItem: suggestionsCollectionView, attribute: .leading, multiplier: 1.0, constant: 0)
+        ConstraintUtils.addActiveConstraint(item: self.view, attribute: .trailing, relatedBy: .equal, toItem: suggestionsCollectionView, attribute: .trailing, multiplier: 1.0, constant: 0)
         ConstraintUtils.addActiveConstraint(item: repeatAnalysisButton, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1.0, constant: 60)
     }
     
@@ -122,11 +128,11 @@ extension NoResultsViewController: UICollectionViewDataSource {
 
 extension NoResultsViewController: UICollectionViewDelegateFlowLayout {
     public func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return tipsCollectionView.cellSize()
+        return suggestionsCollectionView.cellSize()
     }
     
     public func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
-        return tipsCollectionView.headerSize()
+        return suggestionsCollectionView.headerSize()
     }
     
     public func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
