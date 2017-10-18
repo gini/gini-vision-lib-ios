43b596b7c413f4f7c639cec195b3e05eb471dd5f
Enrique <enrique0703@gmail.com>
2017-10-18 14:30:56 +0200

    Replaced font with the bold default one.
    Issue #68

--- GiniVision/Classes/CaptureSuggestionsCollectionView.swift
+++ GiniVision/Classes/CaptureSuggestionsCollectionView.swift
@@ -119,7 +119,7 @@ final class CaptureSuggestionsCollectionHeader: UICollectionReusableView {
         let headerTitle = UILabel()
         headerTitle.translatesAutoresizingMaskIntoConstraints = false
         headerTitle.numberOfLines = 0
-        headerTitle.font = UIFont.systemFont(ofSize: 14, weight: 500)
+        headerTitle.font = UIFont.boldSystemFont(ofSize: 14)
         return headerTitle
     }()
     var bottomLine: UIView = {
