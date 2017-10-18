398cbcd3ab8de8daa8a7abcd4cc769c6060e84eb
Enrique <enrique0703@gmail.com>
2017-10-18 16:04:23 +0200

    Removed unused code in Help menu.
    Issue #88

--- GiniVision/Classes/HelpMenuViewController.swift
+++ GiniVision/Classes/HelpMenuViewController.swift
@@ -17,12 +17,7 @@ class HelpMenuViewController: UITableViewController {
         tableView.tableFooterView = UIView()
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: reuseIdentifier)
         tableView.rowHeight = 64
-        tableView.backgroundColor = UIColor.white.withAlphaComponent(0.95)
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        tableView.backgroundColor = Colors.Gini.pearl
     }
 
     // MARK: - Table view data source
@@ -42,59 +37,14 @@ class HelpMenuViewController: UITableViewController {
         cell.textLabel?.text = strings[indexPath.row]
         cell.textLabel?.font = cell.textLabel?.font.withSize(14)
         cell.accessoryType = .disclosureIndicator
-        cell.backgroundColor = 
+        cell.backgroundColor = .white
         
         return cell
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        // Here should switch between different view controllers.
         navigationController?.pushViewController(HelpMenuViewController(), animated: true)
-        tableView.deselectRow(at: indexPath, animated: true)
     }
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
