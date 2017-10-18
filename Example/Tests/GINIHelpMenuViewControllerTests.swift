3f450d8faea5700a0a29a912945fcc2e5eba62c6
Enrique <enrique0703@gmail.com>
2017-10-18 17:27:35 +0200

    Added test class and change some methods on help menu.
    Issue #88

---
+++ Example/Tests/GINIHelpMenuViewControllerTests.swift
@@ -0,0 +1,59 @@
+//
+//  GINIHelpMenuViewControllerTests.swift
+//  GiniVision_Tests
+//
+//  Created by Enrique del Pozo Gómez on 10/18/17.
+//  Copyright © 2017 CocoaPods. All rights reserved.
+//
+
+import XCTest
+@testable import GiniVision
+
+class GINIHelpMenuViewControllerTests: XCTestCase {
+    
+    let helpMenuViewController: HelpMenuViewController = HelpMenuViewController(style: .plain)
+    
+    override func setUp() {
+        super.setUp()
+        _ = helpMenuViewController.view
+    }
+    
+    func testNumberOfSections() {
+        let numberOfSections = helpMenuViewController.tableView.numberOfSections
+        
+        XCTAssertEqual(numberOfSections, 1, "The number of sections of the table should be always 1")
+    }
+    
+    func testNumberOfRowsInSection0() {
+        let itemsCount = helpMenuViewController.items.count
+        
+        let tableRowsCount = helpMenuViewController.tableView.numberOfRows(inSection: 0)
+        
+        XCTAssertEqual(itemsCount, tableRowsCount, "the number of rows should be equal to the datasource items count")
+    }
+    
+    func testCellTextAtIndexPath() {
+        let indexPath = IndexPath(row: 0, section: 0)
+        let itemText = helpMenuViewController.items[indexPath.row].text
+        
+        let cellTextAtIndexPath = helpMenuViewController.tableView(helpMenuViewController.tableView, cellForRowAt: indexPath).textLabel?.text
+        
+        XCTAssertEqual(itemText, cellTextAtIndexPath, "cell text in the first row should be the same as the first item text")
+        
+    }
+    
+    func testTableRowheight() {
+        let tableRowHeight = helpMenuViewController.tableView.rowHeight
+        
+        XCTAssertEqual(tableRowHeight, helpMenuViewController.tableRowHeight, "table row height should be the one declared on the initialization")
+    }
+    
+    func testNoViewControllerForUnknownID() {
+        let unknownID = -1
+        
+        let viewController = helpMenuViewController.viewController(forRowWithId: unknownID)
+        
+        XCTAssertNil(viewController, "should be nil since none viewController on the table has that ID")
+    }
+    
+}
--- GiniVision/Classes/HelpMenuViewController.swift
+++ GiniVision/Classes/HelpMenuViewController.swift
@@ -8,33 +8,36 @@
 import UIKit
 
 class HelpMenuViewController: UITableViewController {
-
+    
+    let tableRowHeight: CGFloat = 64
     var reuseIdentifier = "reuseIdentifier"
-    var strings: [String] = ["Tipps für beste Ergebnisse aus Fotos", "Dokumente aus anderen Apps öffnen", "Unterstützte Formate"]
+    var items: [(text: String, id: Int)] = [
+        ("Tipps für beste Ergebnisse aus Fotos", 1),
+        ("Dokumente aus anderen Apps öffnen", 2),
+        ("Unterstützte Formate", 3)]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.tableFooterView = UIView()
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: reuseIdentifier)
-        tableView.rowHeight = 64
+        tableView.rowHeight = tableRowHeight
         tableView.backgroundColor = Colors.Gini.pearl
     }
-
+    
     // MARK: - Table view data source
-
+    
     override func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return strings.count
+        return items.count
     }
-
+    
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath)
-
-        cell.textLabel?.text = strings[indexPath.row]
+        cell.textLabel?.text = items[indexPath.row].0
         cell.textLabel?.font = cell.textLabel?.font.withSize(14)
         cell.accessoryType = .disclosureIndicator
         cell.backgroundColor = .white
@@ -43,8 +46,22 @@ class HelpMenuViewController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        // Here should switch between different view controllers.
-        navigationController?.pushViewController(HelpMenuViewController(), animated: true)
+        if let viewController = viewController(forRowWithId: items[indexPath.row].id) {
+            navigationController?.pushViewController(viewController, animated: true)
+        }
     }
-
+    
+    func viewController(forRowWithId id: Int) -> UIViewController? {
+        switch id {
+        case 1:
+            return HelpMenuViewController()
+        case 2:
+            return HelpMenuViewController()
+        case 3:
+            return HelpMenuViewController()
+        default:
+            return nil
+        }
+    }
+    
 }
