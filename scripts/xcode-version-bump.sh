d239a0967d594c48591a9a1a9311d9faa359e01c
Kike <enrique@gini.net>
2017-11-03 14:26:37 +0100

    Added xcode version bump script for Example project.

---
+++ scripts/xcode-version-bump.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+infoPlist="Example/GiniVision/Info.plist"
+
+#Build number bump
+buildNumber=$(/usr/libexec/PlistBuddy -c "Print CFBundleVersion" "$infoPlist")
+buildNumber=$(($buildNumber + 1))
+/usr/libexec/PlistBuddy -c "Set :CFBundleVersion $buildNumber" "$infoPlist"
+
+#Version name bump
+VERSIONNUM=$(/usr/libexec/PlistBuddy -c "Print CFBundleShortVersionString" "$infoPlist")
+
+NEWSUBVERSION=`echo $VERSIONNUM | awk -F "." '{print $3}'`
+NEWSUBVERSION=$(($NEWSUBVERSION + 1))
+
+if [ $NEWSUBVERSION -gt 9 ]
+then
+    NEWSUBVERSION=`echo $VERSIONNUM | awk -F "." '{print $2}'`
+    NEWSUBVERSION=$(($NEWSUBVERSION + 1))
+    if [ $NEWSUBVERSION -gt 9 ]
+    then
+      NEWSUBVERSION=`echo $VERSIONNUM | awk -F "." '{print $1}'`
+      NEWSUBVERSION=$(($NEWSUBVERSION + 1))
+      NEWVERSIONSTRING=`echo $VERSIONNUM | awk -F "." '{print "'$NEWSUBVERSION'." 0 "." 0 }'`
+
+    else
+      NEWVERSIONSTRING=`echo $VERSIONNUM | awk -F "." '{print $1 ".'$NEWSUBVERSION'." 0 }'`
+    fi
+else
+    NEWVERSIONSTRING=`echo $VERSIONNUM | awk -F "." '{print $1 "." $2 ".'$NEWSUBVERSION'" }'`
+fi
+
+/usr/libexec/PlistBuddy -c "Set :CFBundleShortVersionString $NEWVERSIONSTRING" "$infoPlist"
