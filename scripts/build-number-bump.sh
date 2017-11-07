3cbdd5fdd14a5a18fbbd6749d746849efa3beb8a
Kike <enrique@gini.net>
2017-11-07 13:59:56 +0100

    Commited commit changes on script.

--- scripts/build-number-bump.sh
+++ scripts/build-number-bump.sh
@@ -7,6 +7,6 @@ buildNumber=$(/usr/libexec/PlistBuddy -c "Print CFBundleVersion" "$infoPlist")
 buildNumber=$(($buildNumber + 1))
 /usr/libexec/PlistBuddy -c "Set :CFBundleVersion $buildNumber" "$infoPlist"
 
-git add .
-git commit -m "Bumped Example app build number to $buildNumber"
-git push
+# git add .
+# git commit -m "Bumped Example app build number to $buildNumber"
+# git push
