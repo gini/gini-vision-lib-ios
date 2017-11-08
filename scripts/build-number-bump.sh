4943d645e446037d6d57eaf639372034df00a50a
Kike <enrique@gini.net>
2017-11-08 11:18:15 +0100

    Updated build_number_bump script

--- scripts/build-number-bump.sh
+++ scripts/build-number-bump.sh
@@ -2,13 +2,8 @@
 
 infoPlist="Example/GiniVision/Info.plist"
 
-git checkout develop
+hockeyLastBuildNumber=$(curl -H "X-HockeyAppToken: 044a1b5cec5946ff960e9bd646486f27" https://rink.hockeyapp.net/api/2/apps/c9a58ca1b2b14e6d9d5f463ae91d35b6/app_versions | python -c "import sys, json; print json.load(sys.stdin)['app_versions'][0]['version']")
 
 #Build number bump
-buildNumber=$(/usr/libexec/PlistBuddy -c "Print CFBundleVersion" "$infoPlist")
-buildNumber=$(($buildNumber + 1))
+buildNumber=$(($hockeyLastBuildNumber + 1))
 /usr/libexec/PlistBuddy -c "Set :CFBundleVersion $buildNumber" "$infoPlist"
-
-git add .
-git commit -m "Bumped Example app build number to $buildNumber [ci skip]"
-git push
