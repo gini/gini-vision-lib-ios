6a1669100a0f50150f34b25b519b8af660ed0691
Kike <enrique@gini.net>
2017-12-04 16:32:37 +0100

    Removed "multiplier: 1.0"

--- GiniVision/Classes/CaptureSuggestionsCollectionCell.swift
+++ GiniVision/Classes/CaptureSuggestionsCollectionCell.swift
@@ -38,17 +38,17 @@ final class CaptureSuggestionsCollectionCell: UICollectionViewCell {
     }
     
     private func addConstraints() {
-        Contraints.active(item: suggestionImage, attr: .top, relatedBy: .equal, to: self, attr: .top, multiplier: 1.0, priority: 999)
-        Contraints.active(item: suggestionImage, attr: .bottom, relatedBy: .equal, to: self, attr: .bottom, multiplier: 1.0, priority: 999)
-        Contraints.active(item: suggestionImage, attr: .leading, relatedBy: .equal, to: self, attr: .leading, multiplier: 1.0, constant: 20)
-        Contraints.active(item: suggestionImage, attr: .trailing, relatedBy: .equal, to: suggestionText, attr: .leading, multiplier: 1.0, constant: -20)
-        Contraints.active(item: suggestionImage, attr: .width, relatedBy: .equal, to: nil, attr: .notAnAttribute, multiplier: 1.0, constant: 85)
-        Contraints.active(item: suggestionImage, attr: .height, relatedBy: .lessThanOrEqual, to: nil, attr: .notAnAttribute, multiplier: 1.0, constant: 85)
-        Contraints.active(item: suggestionImage, attr: .centerY, relatedBy: .equal, to: self, attr: .centerY, multiplier: 1.0)
+        Contraints.active(item: suggestionImage, attr: .top, relatedBy: .equal, to: self, attr: .top, priority: 999)
+        Contraints.active(item: suggestionImage, attr: .bottom, relatedBy: .equal, to: self, attr: .bottom, priority: 999)
+        Contraints.active(item: suggestionImage, attr: .leading, relatedBy: .equal, to: self, attr: .leading, constant: 20)
+        Contraints.active(item: suggestionImage, attr: .trailing, relatedBy: .equal, to: suggestionText, attr: .leading, constant: -20)
+        Contraints.active(item: suggestionImage, attr: .width, relatedBy: .equal, to: nil, attr: .notAnAttribute, constant: 85)
+        Contraints.active(item: suggestionImage, attr: .height, relatedBy: .lessThanOrEqual, to: nil, attr: .notAnAttribute, constant: 85)
+        Contraints.active(item: suggestionImage, attr: .centerY, relatedBy: .equal, to: self, attr: .centerY)
         
-        Contraints.active(item: suggestionText, attr: .top, relatedBy: .equal, to: self, attr: .top, multiplier: 1.0)
-        Contraints.active(item: suggestionText, attr: .bottom, relatedBy: .equal, to: self, attr: .bottom, multiplier: 1.0)
-        Contraints.active(item: suggestionText, attr: .trailing, relatedBy: .equal, to: self, attr: .trailing, multiplier: 1.0, constant: -20, priority: 999)
+        Contraints.active(item: suggestionText, attr: .top, relatedBy: .equal, to: self, attr: .top)
+        Contraints.active(item: suggestionText, attr: .bottom, relatedBy: .equal, to: self, attr: .bottom)
+        Contraints.active(item: suggestionText, attr: .trailing, relatedBy: .equal, to: self, attr: .trailing, constant: -20, priority: 999)
     }
 }
 
