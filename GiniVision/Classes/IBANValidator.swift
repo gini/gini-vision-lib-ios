54ca48c448f7d2dd9adacd7e2c6a3c5d5a397f42
Kike <enrique@gini.net>
2017-12-05 17:34:49 +0100

    Fixed deprecated code in IbanValidator

--- GiniVision/Classes/IBANValidator.swift
+++ GiniVision/Classes/IBANValidator.swift
@@ -35,11 +35,11 @@ final class IBANValidator {
             return false
         }
         
-        if let _ = iban.rangeOfCharacter(from: validationSet) {
+        if iban.rangeOfCharacter(from: validationSet) != nil {
             return false
         }
         
-        let countryCode = iban.substring(to: iban.startIndex.advanced(by: 2))
+        let countryCode = iban.substring(to: iban.index(iban.startIndex, offsetBy: 2))
         let countryDescriptor = countryIbanDictionary[countryCode]
         var countryIsValid = false
         if let countryDescriptor = countryDescriptor {
@@ -49,7 +49,8 @@ final class IBANValidator {
             }
         }
         
-        let normalizedIban = "\(iban.substring(from: iban.startIndex.advanced(by: 4)))\(iban.substring(to: iban.startIndex.advanced(by: 4)))"
+        let normalizedIban = "\(iban.substring(from: iban.index(iban.startIndex, offsetBy: 4)))" +
+        "\(iban.substring(to: iban.index(iban.startIndex, offsetBy: 4)))"
         
         let result = validateMod97(iban: normalizedIban)
         if !countryIsValid && result == true {
