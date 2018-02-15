46aff1359139cbd76ee9a5ee67e392d260af7e79
Kike <enrique@gini.net>
2018-02-15 17:23:47 +0100

    Added Extraction typealias.

--- GiniVision/Classes/Networking/GiniVision+GiniVisionDelegate.swift
+++ GiniVision/Classes/Networking/GiniVision+GiniVisionDelegate.swift
@@ -12,8 +12,6 @@ extension GiniVision {
                                      importedDocument: GiniVisionDocument? = nil,
                                      giniConfiguration: GiniConfiguration,
                                      resultsDelegate: GiniVisionResultsDelegate) -> UIViewController {
-        print("Gini Vision Library for iOS (\(GiniVision.versionString)) / Client id: \(client.clientId)")
-
         GiniVision.setConfiguration(giniConfiguration)
         let screenCoordinator = GiniScreenAPICoordinator(client: client,
                                                          resultsDelegate: resultsDelegate,
