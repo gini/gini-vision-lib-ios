3b6a374a69cb9725d9d73b456b98cedb02b8ace6
Kike <enrique@gini.net>
2018-02-16 09:03:13 +0100

    Simplified APIService

--- Example/GiniVision/AppCoordinator.swift
+++ Example/GiniVision/AppCoordinator.swift
@@ -49,7 +49,7 @@ final class AppCoordinator: Coordinator {
     let clientPassword = "client_password"
     let clientEmailDomain = "client_email_domain"
     
-    private lazy var client: Client = {
+    private lazy var client: GiniClient = {
         var keys: NSDictionary?
         if let path = Bundle.main.path(forResource: "Credentials", ofType: "plist"),
             let keys = NSDictionary(contentsOfFile: path),
@@ -58,11 +58,11 @@ final class AppCoordinator: Coordinator {
             let client_email_domain = keys[self.clientEmailDomain] as? String,
             !client_id.isEmpty, !client_password.isEmpty, !client_email_domain.isEmpty {
             
-            return Client(clientId: client_id,
+            return GiniClient(clientId: client_id,
                           clientSecret: client_password,
                           clientEmailDomain: client_email_domain)
         }
-        return Client(clientId: "",
+        return GiniClient(clientId: "",
                       clientSecret: "",
                       clientEmailDomain: "")
     }()
