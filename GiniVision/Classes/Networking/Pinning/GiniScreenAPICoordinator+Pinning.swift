2f883c7d958fa7e44d25853623ec1e3aaf10ddce
Kike <enrique@gini.net>
2018-02-16 10:41:19 +0100

    Added public key pinning methods.

---
+++ GiniVision/Classes/Networking/Pinning/GiniScreenAPICoordinator+Pinning.swift
@@ -0,0 +1,27 @@
+//
+//  GiniScreenAPICoordinator+Pinning.swift
+//  GiniVision
+//
+//  Created by Enrique del Pozo Gómez on 2/16/18.
+//
+
+import Foundation
+import Gini_iOS_SDK
+
+extension GiniScreenAPICoordinator {
+    convenience init(client: GiniClient,
+                     resultsDelegate: GiniVisionResultsDelegate,
+                     giniConfiguration: GiniConfiguration,
+                     publicKeyPinningConfig: [String: Any]) {
+        self.init(withDelegate: nil,
+                  giniConfiguration: giniConfiguration)
+        self.visionDelegate = self
+        self.resultsDelegate = resultsDelegate
+        
+        let builder = GINISDKBuilder.anonymousUser(withClientID: client.clientId,
+                                                   clientSecret: client.clientSecret,
+                                                   userEmailDomain: client.clientEmailDomain,
+                                                   publicKeyPinningConfig: publicKeyPinningConfig)
+        self.apiService = APIService(sdk: builder?.build())
+    }
+}
