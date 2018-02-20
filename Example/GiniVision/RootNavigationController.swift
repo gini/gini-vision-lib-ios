f2dc4f97d3ecac62d50c86d09d8892999c8e091c
Kike <enrique@gini.net>
2018-02-20 13:50:13 +0100

    Add explanation on RootNavigationController. Restructured project.

--- Example/GiniVision/RootNavigationController.swift
+++ Example/GiniVision/RootNavigationController.swift
@@ -8,6 +8,13 @@
 
 import UIKit
 
+/**
+ This wraps up the Screen API in a UINavigationController,
+ to make the transition to the results screen (external screen) easier.
+ This is only used as an example and is not mandatory to implement, it depends
+ on how the Screen API and further screens are shown.
+ */
+
 final class RootNavigationController: UINavigationController {
     override var shouldAutorotate: Bool {
         return self.topViewController?.shouldAutorotate ?? true
