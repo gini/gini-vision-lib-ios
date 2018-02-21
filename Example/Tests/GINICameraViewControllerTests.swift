8a7921a9b687d7bd6e143e397af208bea44a1253
Kike <enrique@gini.net>
2018-02-21 17:56:28 +0100

    Fixed issues with tests and removed unused code and inappropriate tests.
    Issue #143

--- Example/Tests/GINICameraViewControllerTests.swift
+++ Example/Tests/GINICameraViewControllerTests.swift
@@ -53,39 +53,5 @@ final class CameraViewControllerTests: XCTestCase {
                       "multipageReviewBackgroundView should not be hidden after capture the second picture")
         
     }
-    
-    func testReviewImagesButtonTouchInteraction() {
-        _ = vc.view
-        
-        let image = loadImage(withName: "invoice.jpg")
-        let imageData = UIImageJPEGRepresentation(image!, 0.9)
-        XCTAssertFalse(vc.multipageReviewButton.isUserInteractionEnabled,
-                       "multipageReviewButton should be disabled on start")
-        
-        vc.cameraDidCapture(imageData: imageData, error: nil)
-        
-        let predicate = NSPredicate(format: "isUserInteractionEnabled == YES")
-        
-        _ = self.expectation(for: predicate,
-                             evaluatedWith: vc.multipageReviewButton,
-                             handler: .none)
-        waitForExpectations(timeout: 1.5, handler: { result in
-            XCTAssertNil(result, "multipageReviewButton should be enabled after capturing a picture")
-            XCTAssertTrue(self.vc.multipageReviewBackgroundView.isHidden,
-                          "multipageReviewBackgroundView should be hidden before capture the first picture")
-            self.vc.cameraDidCapture(imageData: imageData, error: nil)
-            
-            let predicate = NSPredicate(format: "isHidden == NO")
-            
-            _ = self.expectation(for: predicate,
-                                 evaluatedWith: self.vc.multipageReviewBackgroundView,
-                                 handler: .none)
-            self.waitForExpectations(timeout: 1.5, handler: { result in
-                XCTAssertFalse(self.vc.multipageReviewBackgroundView.isHidden,
-                              "multipageReviewBackgroundView should not be hidden after capture the second picture")
-            })
-        })
-        
-    }
 }
 
