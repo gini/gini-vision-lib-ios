2e04518fbe74af2e97255730e8b566c78b0a042b
Kike <enrique@gini.net>
2018-02-26 16:45:12 +0100

    Added missing files.
    Issue #156

--- GiniVision/Classes/Core/GiniAlbumsPickerViewController.swift
+++ GiniVision/Classes/Core/GiniAlbumsPickerViewController.swift
@@ -7,3 +7,54 @@
 
 import Foundation
 
+final class GiniAlbumsPickerViewController: UIViewController {
+    
+    let galleryManager: GiniGalleryImageManagerProtocol
+    lazy var albumsTableView: UITableView = {
+        let tableView = UITableView(frame: .zero, style: .plain)
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.register(UITableViewCell.self,
+                           forCellReuseIdentifier: "GiniAlbumsPickerTableViewCellIdentifier")
+        return tableView
+    }()
+    
+    init(galleryManager: GiniGalleryImageManagerProtocol) {
+        self.galleryManager = galleryManager
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func loadView() {
+        super.loadView()
+        
+        view.addSubview(albumsTableView)
+        Constraints.pin(view: albumsTableView, toSuperView: view)
+    }
+    
+}
+
+// MARK: UITableViewDataSource
+
+extension GiniAlbumsPickerViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "GiniAlbumsPickerTableViewCellIdentifier")
+        return cell!
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+}
+
+// MARK: UITableViewDelegate
+
+extension GiniAlbumsPickerViewController: UITableViewDelegate {
+    
+}
