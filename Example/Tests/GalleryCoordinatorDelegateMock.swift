1485d4f593dca2a99ee6d3b70ee7d4c6bba631b3
Kike <enrique@gini.net>
2018-03-02 14:01:49 +0100

    Added missing tests for GalleryCoordinator.
    Issue #156

---
+++ Example/Tests/GalleryCoordinatorDelegateMock.swift
@@ -0,0 +1,26 @@
+//
+//  GalleryCoordinatorDelegateMock.swift
+//  GiniVision_Tests
+//
+//  Created by Enrique del Pozo Gómez on 3/2/18.
+//  Copyright © 2018 Gini GmbH. All rights reserved.
+//
+
+import Foundation
+@testable import GiniVision
+
+final class GalleryCoordinatorDelegateMock: GalleryCoordinatorDelegate {
+    
+    var didCancelGallery = false
+    var didOpenImages = false
+    var openedImageDocuments: [GiniImageDocument] = []
+    
+    func gallery(_ coordinator: GalleryCoordinator, didCancel: Void) {
+        didCancelGallery = true
+    }
+    
+    func gallery(_ coordinator: GalleryCoordinator, didSelectImageDocuments imageDocuments: [GiniImageDocument]) {
+        didOpenImages = true
+        openedImageDocuments = imageDocuments
+    }
+}
