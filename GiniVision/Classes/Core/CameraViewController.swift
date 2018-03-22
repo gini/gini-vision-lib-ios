9de318cb6a613b270dd26f31775d49b33dc3b821
Kike <enrique@gini.net>
2018-03-22 15:17:55 +0100

    Fixed issue with gallery selection state.
    Issue #178

--- GiniVision/Classes/Core/CameraViewController.swift
+++ GiniVision/Classes/Core/CameraViewController.swift
@@ -700,7 +700,6 @@ extension CameraViewController: DocumentPickerCoordinatorDelegate {
 extension CameraViewController {
     fileprivate func enableFileImport() {
         // Configure file picker
-        documentPickerCoordinator.startCaching()
         documentPickerCoordinator.delegate = self
 
         if documentPickerCoordinator.isGalleryPermissionGranted {
