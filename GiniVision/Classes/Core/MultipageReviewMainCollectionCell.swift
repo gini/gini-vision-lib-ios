f6536613fbec8e56ccb8f0e3d72b8ad3b099c620
Kike <enrique@gini.net>
2018-04-13 16:26:12 +0200

    Added zooming in multipage review screen collection.

--- GiniVision/Classes/Core/MultipageReviewMainCollectionCell.swift
+++ GiniVision/Classes/Core/MultipageReviewMainCollectionCell.swift
@@ -19,15 +19,55 @@ final class MultipageReviewMainCollectionCell: UICollectionViewCell {
         return imageView
     }()
     
+    lazy var zoomableScrollView: UIScrollView = {
+        let scrollView = UIScrollView()
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        scrollView.delegate = self
+        scrollView.minimumZoomScale = 1
+        scrollView.maximumZoomScale = 5
+
+        return scrollView
+    }()
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
-        self.addSubview(documentImage)
-
-        Constraints.pin(view: documentImage, toSuperView: self)
+        zoomableScrollView.addSubview(documentImage)
+        addSubview(zoomableScrollView)
+        addConstraints()
+        addDoubleTapGesture()
     }
     
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(frame:) has not been implemented")
     }
     
+    private func addConstraints() {
+        Constraints.pin(view: documentImage, toSuperView: zoomableScrollView)
+        Constraints.pin(view: zoomableScrollView, toSuperView: self)
+        Constraints.center(view: documentImage, with: zoomableScrollView)
+    }
+    
+    private func addDoubleTapGesture() {
+        let tapGesture = UITapGestureRecognizer()
+        tapGesture.numberOfTapsRequired = 2
+        tapGesture.addTarget(self, action: #selector(doubleTapToZoom))
+        zoomableScrollView.addGestureRecognizer(tapGesture)
+    }
+    
+    @objc private func doubleTapToZoom() {
+        if zoomableScrollView.zoomScale == 1.0 {
+            zoomableScrollView.setZoomScale(2.0, animated: true)
+        } else {
+            zoomableScrollView.setZoomScale(1.0, animated: true)
+        }
+    }
+    
+}
+
+// MARK: - UIScrollViewDelegate
+
+extension MultipageReviewMainCollectionCell: UIScrollViewDelegate {
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        return documentImage
+    }
 }
