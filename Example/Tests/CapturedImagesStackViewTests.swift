99d20a4a0a706a45d1b3da50120450a93721ba2b
Kike <enrique@gini.net>
2018-05-07 15:38:38 +0200

    Split tests for CaptureImagesStackView tests.

--- Example/Tests/CapturedImagesStackViewTests.swift
+++ Example/Tests/CapturedImagesStackViewTests.swift
@@ -26,7 +26,7 @@ final class CapturedImagesStackViewTests: XCTestCase {
         
     }
     
-    func testCaptureStackWhenOneImageCaptured() {
+    func testCaptureStackVisibilityWhenOneImageCaptured() {
         let images = [loadImage(withName: "invoice.jpg")!]
         
         capturedImagesStackView.replaceStackImages(with: images)
@@ -35,23 +35,27 @@ final class CapturedImagesStackViewTests: XCTestCase {
                       "capturedImagesStackView should not be hidden when it is filled")
         XCTAssertTrue(capturedImagesStackView.thumbnailStackBackgroundView.isHidden,
                       "thumbnailStackBackgroundView should be hidden when there is only 1 image")
-        XCTAssertEqual(capturedImagesStackView.thumbnailButton.image(for: .normal), images[0],
-                       "thumbnailButton image should match last image in array")
         
+    }
+    
+    func testCaptureStackVisibilityWhenTwoImageCaptured() {
+        let images = [loadImage(withName: "invoice.jpg")!,
+                      loadImage(withName: "invoice2.jpg")!]
+        capturedImagesStackView.replaceStackImages(with: images)
+
+        XCTAssertFalse(capturedImagesStackView.thumbnailStackBackgroundView.isHidden,
+                       "thumbnailStackBackgroundView should not be hidden when there are 2 images")
+
     }
     
     func testCaptureStackWhenTwoImageCaptured() {
         let images = [loadImage(withName: "invoice.jpg")!,
                       loadImage(withName: "invoice2.jpg")!]
         capturedImagesStackView.replaceStackImages(with: images)
-        
-        XCTAssertFalse(capturedImagesStackView.isHidden,
-                       "capturedImagesStackView should not be hidden when it is filled")
-        XCTAssertFalse(capturedImagesStackView.thumbnailStackBackgroundView.isHidden,
-                       "thumbnailStackBackgroundView should not be hidden when there are 2 images")
+
         XCTAssertEqual(capturedImagesStackView.thumbnailButton.image(for: .normal), images[1],
                        "thumbnailButton image should match last image in array")
-
+        
     }
     
 }
