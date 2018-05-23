55126883ef1fdd653c9e683010f69ce93689e0f2
Kike <enrique@gini.net>
2018-05-23 10:06:12 +0200

    Added customization for stack images view label indicator color.

--- GiniVision/Classes/Core/CameraViewController.swift
+++ GiniVision/Classes/Core/CameraViewController.swift
@@ -137,7 +137,7 @@ public typealias CameraScreenFailureBlock = (_ error: GiniVisionError) -> Void
         return label
     }()
     lazy var capturedImagesStackView: CapturedImagesStackView = {
-        let view = CapturedImagesStackView(frame: .zero)
+        let view = CapturedImagesStackView(giniConfiguration: giniConfiguration)
         view.translatesAutoresizingMaskIntoConstraints = false
         view.isHidden = true
         view.didTapImageStackButton = { [weak self] in
