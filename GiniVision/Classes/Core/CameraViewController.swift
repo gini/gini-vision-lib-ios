54f2f218c1200c5a9dfd041d767952c3b590a2fc
Kike <enrique@gini.net>
2018-05-29 15:01:45 +0200

    Added GiniConfiguration deprecations

--- GiniVision/Classes/Core/CameraViewController.swift
+++ GiniVision/Classes/Core/CameraViewController.swift
@@ -540,7 +540,7 @@ extension CameraViewController {
     
     @objc fileprivate func captureImage(_ sender: AnyObject) {
         guard let camera = camera else {
-            Logger.debug(message: "No camera initialized", event: .warning)
+            Log(message: "No camera initialized", event: .warning)
             return
         }
         
