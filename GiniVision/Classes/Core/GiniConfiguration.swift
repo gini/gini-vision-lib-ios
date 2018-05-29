e2b540a8d4f046bbfe93e2631c07520ae515a0ed
Kike <enrique@gini.net>
2018-05-29 15:18:58 +0200

    Added warning in depercated strings in GiniConfiguration

--- GiniVision/Classes/Core/GiniConfiguration.swift
+++ GiniVision/Classes/Core/GiniConfiguration.swift
@@ -660,7 +660,6 @@ import UIKit
      - Warning: Deprecated, use the ginivision.analysis.loadingText localized string instead
 
      */
-    @available(*, deprecated, message: "Use ginivision.analysis.loadingText localized string instead")
     @objc public var analysisLoadingText =
         NSLocalizedStringPreferred("ginivision.analysis.loadingText",
                                    comment: "Text appearing at the center of the analysis screen " +
