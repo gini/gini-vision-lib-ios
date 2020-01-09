cc861b3a9f497325bd195c007db68aaebcc77b64
maciejtrybilo <maciej@gini.net>
2020-01-09 09:53:58 +0100

    Use NSAttributedString on one label rather than two labels to display the total price. Also, include the word "Total" in the accessibility label for that.

--- GiniVision/Classes/Core/Screens/Digital Invoice/DigitalInvoiceTotalPriceCell.swift
+++ GiniVision/Classes/Core/Screens/Digital Invoice/DigitalInvoiceTotalPriceCell.swift
@@ -11,8 +11,7 @@ class DigitalInvoiceTotalPriceCell: UITableViewCell {
     
     var giniConfiguration = GiniConfiguration.shared
     
-    private var totalPriceMainUnitLabel: UILabel?
-    private var totalPriceFractionalUnitLabel: UILabel?
+    private var totalPriceLabel = UILabel()
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
@@ -30,9 +29,21 @@ class DigitalInvoiceTotalPriceCell: UITableViewCell {
         didSet {
             
             guard let totalPrice = totalPrice else { return }
-                        
-            totalPriceMainUnitLabel?.text = totalPrice.mainUnitComponentString
-            totalPriceFractionalUnitLabel?.text = totalPrice.fractionalUnitComponentString
+            
+            let totalPriceString = "€\(totalPrice.string)"
+            
+            let attributedString =
+                NSMutableAttributedString(string: totalPriceString,
+                                          attributes: [NSAttributedString.Key.foregroundColor: giniConfiguration.digitalInvoiceTotalPriceColor,
+                                                       NSAttributedString.Key.font: giniConfiguration.digitalInvoiceTotalPriceMainUnitFont])
+            
+            attributedString.setAttributes([NSAttributedString.Key.foregroundColor: giniConfiguration.digitalInvoiceTotalPriceColor,
+                                            NSAttributedString.Key.baselineOffset: 9,
+                                            NSAttributedString.Key.font: giniConfiguration.digitalInvoiceTotalPriceFractionalUnitFont],
+                                           range: NSRange(location: totalPriceString.count - 3, length: 3))
+            
+            totalPriceLabel.attributedText = attributedString
+            totalPriceLabel.accessibilityLabel = "Total: \(totalPriceString)"
         }
     }
     
@@ -40,29 +51,11 @@ class DigitalInvoiceTotalPriceCell: UITableViewCell {
         
         selectionStyle = .none
         
-        let totalPriceMainUnitLabel = UILabel()
-        totalPriceMainUnitLabel.translatesAutoresizingMaskIntoConstraints = false
-        totalPriceMainUnitLabel.font = giniConfiguration.digitalInvoiceTotalPriceMainUnitFont
+        totalPriceLabel.translatesAutoresizingMaskIntoConstraints = false
+        contentView.addSubview(totalPriceLabel)
         
-        self.totalPriceMainUnitLabel = totalPriceMainUnitLabel
-        
-        let totalPriceFractionalUnitLabel = UILabel()
-        totalPriceFractionalUnitLabel.translatesAutoresizingMaskIntoConstraints = false
-        totalPriceFractionalUnitLabel.font = giniConfiguration.digitalInvoiceTotalPriceFractionalUnitFont
-        
-        self.totalPriceFractionalUnitLabel = totalPriceFractionalUnitLabel
-        
-        contentView.addSubview(totalPriceMainUnitLabel)
-        contentView.addSubview(totalPriceFractionalUnitLabel)
-        
-        totalPriceFractionalUnitLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor,
-                                                       constant: -16).isActive = true
-        totalPriceFractionalUnitLabel.topAnchor.constraint(equalTo: totalPriceMainUnitLabel.topAnchor,
-                                                           constant: 3).isActive = true
-        
-        totalPriceMainUnitLabel.trailingAnchor.constraint(equalTo: totalPriceFractionalUnitLabel.leadingAnchor,
-                                                      constant: 0).isActive = true
-        totalPriceMainUnitLabel.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 10).isActive = true
-        totalPriceMainUnitLabel.bottomAnchor.constraint(equalTo: contentView.bottomAnchor).isActive = true
+        totalPriceLabel.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 10).isActive = true
+        totalPriceLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -16).isActive = true
+        totalPriceLabel.bottomAnchor.constraint(equalTo: contentView.bottomAnchor).isActive = true
     }
 }
