f9c6ad97b54ff601ff686f48ac28695321e79526
Nadya Karaban <nadya@gini.net>
2021-08-30 15:44:48 +0200

    iOS 15 : navigation bar title text attributes are set in appearance (PIA-1652)

--- GiniVision/Classes/Core/Extensions/UINavigationController.swift
+++ GiniVision/Classes/Core/Extensions/UINavigationController.swift
@@ -11,21 +11,20 @@ import Foundation
 extension UINavigationController {
     func applyStyle(withConfiguration configuration: GiniConfiguration) {
         self.navigationBar.isTranslucent = false
+        let titleTextAttrubutes = [NSAttributedString.Key.font: configuration.customFont.isEnabled ?
+                              configuration.customFont.with(weight: .light, size: 16, style: .title2) :
+                              configuration.navigationBarTitleFont as Any, NSAttributedString.Key.foregroundColor: configuration.navigationBarTitleColor]
         if #available(iOS 15.0, *) {
             let appearance = UINavigationBarAppearance()
             appearance.configureWithOpaqueBackground()
             appearance.backgroundColor = configuration.navigationBarTintColor
+            appearance.titleTextAttributes = titleTextAttrubutes
             navigationBar.standardAppearance = appearance
             navigationBar.scrollEdgeAppearance = navigationBar.standardAppearance
         } else {
             self.navigationBar.barTintColor = configuration.navigationBarTintColor
+            self.navigationBar.titleTextAttributes = titleTextAttrubutes
         }
         self.navigationBar.tintColor = configuration.navigationBarItemTintColor
-        var attributes = self.navigationBar.titleTextAttributes ?? [NSAttributedString.Key: Any]()
-        attributes[NSAttributedString.Key.foregroundColor] = configuration.navigationBarTitleColor
-        attributes[NSAttributedString.Key.font] = configuration.customFont.isEnabled ?
-            configuration.customFont.with(weight: .light, size: 16, style: .title2) :
-            configuration.navigationBarTitleFont
-        self.navigationBar.titleTextAttributes = attributes
     }
 }
